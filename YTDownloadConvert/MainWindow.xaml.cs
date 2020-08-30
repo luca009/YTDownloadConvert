41a2a1034eab9d36e00091a39405ef9c13571f80
luca009 <realluca009@gmail.com>
2020-08-30 11:07:26 +0200

    Add project files.

---
+++ YTDownloadConvert/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reflection;
+using System.IO;
+using NYoutubeDL;
+using Xabe.FFmpeg;
+
+namespace YTDownloadConvert
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void bGo_Click(object sender, RoutedEventArgs e)
+        {
+            var downloader = new YoutubeDL();
+            //downloader.Options.PostProcessingOptions;
+            downloader.Options.FilesystemOptions.Output = "C:\\Users\\reall\\Downloads\\video.mp4";
+            downloader.YoutubeDlPath = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\youtube-dl.exe";
+
+            lDownloading.Visibility = Visibility.Visible;
+
+            await downloader.DownloadAsync(tbLink.Text);
+
+            lDownloading.Visibility = Visibility.Hidden;
+        }
+    }
+}
