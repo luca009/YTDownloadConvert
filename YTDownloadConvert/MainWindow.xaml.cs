ee52aa86ac20ea3b2c529f8830317b3a58f262c5
luca009 <realluca009@gmail.com>
2020-10-17 12:15:52 +0200

    Threading

--- YTDownloadConvert/MainWindow.xaml.cs
+++ YTDownloadConvert/MainWindow.xaml.cs
@@ -34,7 +34,7 @@ namespace YTDownloadConvert
     {
         string runningPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         string filePath;
-        YoutubeDL downloader = new YoutubeDL();
+        
 
         public MainWindow()
         {
@@ -49,82 +49,111 @@ namespace YTDownloadConvert
 
         private async void bGo_Click(object sender, RoutedEventArgs e)
         {
-            textStatus.Text = "Preparing";
-            bGo.IsEnabled = false;
-            Dispatcher.Invoke(new Action(() => { }), DispatcherPriority.Render);
-            downloader = new YoutubeDL();
-            string fileName = "";
-            downloader.VideoUrl = tbLink.Text;
-            downloader.Options.FilesystemOptions.NoMtime = true;
-            //downloader.Options.VideoFormatOptions.Format = NYoutubeDL.Helpers.Enums.VideoFormat.best;
-            downloader.Options.VerbositySimulationOptions.CallHome = false;
-            downloader.Options.GeneralOptions.Update = true;
-            //downloader.Options.VideoFormatOptions.FormatAdvanced = 
-            string videoTitleNoFilter = downloader.GetDownloadInfo().Title;
-            string videoTitle = Regex.Replace(videoTitleNoFilter, @"[^a-z,0-9, ,-]", "", RegexOptions.IgnoreCase);
-            Random random = new Random();
-            int randomInt = random.Next(1, 1000);
-            downloader.Options.FilesystemOptions.Output = filePath + $"\\video{randomInt}";
-            if (cboxCut.IsChecked == false && cboxConvert.IsChecked == false)
-                downloader.Options.FilesystemOptions.Output = filePath + $"\\{videoTitle}";
-            downloader.YoutubeDlPath = runningPath + "\\youtube-dl.exe";
-            if (rbBest.IsChecked == true)
-                downloader.Options.VideoFormatOptions.FormatAdvanced = "best";
-            else
-                downloader.Options.VideoFormatOptions.FormatAdvanced = "worst";
-            if (cboxResolution.Text != "Auto")
-                downloader.Options.VideoFormatOptions.FormatAdvanced += $"[height <=? {Int32.Parse(cboxResolution.Text.TrimEnd('p'))}]";
-
-            textStatus.Text = "Downloading";
-            pbProgress.Visibility = Visibility.Visible;
-            downloader.DownloadAsync();
-            while (downloader.IsDownloading)
+            await Task.Run(async () =>
             {
-                pbProgress.Value = downloader.Info.VideoProgress;
-                textETA.Text = "ETA: " + downloader.Info.Eta;
-                textSpeed.Text = "Speed: " + downloader.Info.DownloadRate;
-                Dispatcher.Invoke(new Action(() => { }), DispatcherPriority.Render);
-            }
-            pbProgress.Value = 0;
-            textETA.Text = "ETA: ?";
-            textSpeed.Text = "Speed: ?";
-            Dispatcher.Invoke(new Action(() => { }), DispatcherPriority.Render);
-            foreach (string file in Directory.GetFiles(filePath))
-                if (Path.GetFileName(file).StartsWith($"video{randomInt}"))
-                    fileName = file;
+                YoutubeDL downloader = new YoutubeDL();
+                Dispatcher.Invoke(() => {
+                    textStatus.Text = "Preparing";
+                    bGo.IsEnabled = false;
+                });
+                //Dispatcher.Invoke(new Action(() => { }), DispatcherPriority.Render);
+                downloader = new YoutubeDL();
+                string fileName = "";
+                Dispatcher.Invoke(() => { downloader.VideoUrl = tbLink.Text; });
+                downloader.Options.FilesystemOptions.NoMtime = true;
+                //downloader.Options.VideoFormatOptions.Format = NYoutubeDL.Helpers.Enums.VideoFormat.best;
+                downloader.Options.VerbositySimulationOptions.CallHome = false;
+                downloader.Options.GeneralOptions.Update = true;
+                //downloader.Options.VideoFormatOptions.FormatAdvanced = 
+                string videoTitleNoFilter = downloader.GetDownloadInfo().Title;
+                string videoTitle = Regex.Replace(videoTitleNoFilter, @"[^a-z,0-9, ,-]", "", RegexOptions.IgnoreCase);
+                Random random = new Random();
+                int randomInt = random.Next(1, 1000);
+                downloader.Options.FilesystemOptions.Output = filePath + $"\\video{randomInt}";
+                Dispatcher.Invoke(() => {
+                    if (cboxCut.IsChecked == false && cboxConvert.IsChecked == false)
+                        downloader.Options.FilesystemOptions.Output = filePath + $"\\{videoTitle}";
+                    downloader.YoutubeDlPath = runningPath + "\\youtube-dl.exe";
+                    if (rbBest.IsChecked == true)
+                        downloader.Options.VideoFormatOptions.FormatAdvanced = "best";
+                    else
+                        downloader.Options.VideoFormatOptions.FormatAdvanced = "worst";
+                    if (cboxResolution.Text != "Auto")
+                        downloader.Options.VideoFormatOptions.FormatAdvanced += $"[height <=? {Int32.Parse(cboxResolution.Text.TrimEnd('p'))}]";
+                    textStatus.Text = "Downloading";
+                    pbProgress.Visibility = Visibility.Visible;
+                });
+                downloader.DownloadAsync();
+                while (downloader.IsDownloading)
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        pbProgress.Value = downloader.Info.VideoProgress;
+                        textETA.Text = "ETA: " + downloader.Info.Eta;
+                        textSpeed.Text = "Speed: " + downloader.Info.DownloadRate;
+                    });
+                    
+                    //Dispatcher.Invoke(new Action(() => { }), DispatcherPriority.Render);
+                }
+                Dispatcher.Invoke(() =>
+                {
+                    pbProgress.Value = 0;
+                    textETA.Text = "ETA: ?";
+                    textSpeed.Text = "Speed: ?";
+                });
+                
+                //Dispatcher.Invoke(new Action(() => { }), DispatcherPriority.Render);
+                foreach (string file in Directory.GetFiles(filePath))
+                    if (Path.GetFileName(file).StartsWith($"video{randomInt}"))
+                        fileName = file;
 
-            if (cboxCut.IsChecked == true || cboxConvert.IsChecked == true || cboxBitrate.IsChecked == true)
-            {
-                pbProgress.IsIndeterminate = true;
-                textStatus.Text = "Getting FFmpeg";
-                Dispatcher.Invoke(new Action(() => { }), DispatcherPriority.Render);
-                if (!File.Exists(runningPath + "\\ffmpeg.exe"))
-                    await FFmpegDownloader.GetLatestVersion(FFmpegVersion.Full, runningPath);
-                textStatus.Text = "Processing";
-                Dispatcher.Invoke(new Action(() => { }), DispatcherPriority.Render);
-                var conversions = FFmpeg.Conversions.New();
-                if (cboxCut.IsChecked == true)
-                    conversions.AddParameter($"-ss {TimeSpan.FromSeconds(Int32.Parse(tbFrom.Text))}");
-                conversions.AddParameter($"-i \"{fileName}\"");
-                if (cboxConvert.IsChecked == true)
-                    conversions.SetOutput($"{filePath}\\{videoTitle}{cbConvertType.Text}");
-                else
-                    conversions.SetOutput($"{filePath}\\{videoTitle}{Path.GetExtension(fileName)}");
-                if (cboxCut.IsChecked == true)
-                    conversions.AddParameter($"-to {TimeSpan.FromSeconds(Int32.Parse(tbTo.Text) - Int32.Parse(tbFrom.Text))}");
-                if (cboxBitrate.IsChecked == true)
-                    conversions.AddParameter($"-b {tbBitrate.Text}k");
-                /*
-                if (cboxConvert.IsChecked == true && cboxCut.IsChecked == false)
-                    await conversions.AddParameter($"-i \"{fileName}\" \"{filePath}\\{videoTitle}{cbConvertType.Text}\"").Start();
-                if (cboxConvert.IsChecked == true && cboxCut.IsChecked == true)
-                    await conversions.AddParameter($"-ss {TimeSpan.FromSeconds(Int32.Parse(tbFrom.Text))} -i \"{fileName}\" -to {TimeSpan.FromSeconds(Int32.Parse(tbTo.Text) - Int32.Parse(tbFrom.Text))} -c copy \"{filePath + $"\\{videoTitle}{cbConvertType.Text}"}\"").Start();
-                if (cboxConvert.IsChecked == false && cboxCut.IsChecked == true)
-                    await conversions.AddParameter($"-ss {TimeSpan.FromSeconds(Int32.Parse(tbFrom.Text))} -i \"{fileName}\" -to {TimeSpan.FromSeconds(Int32.Parse(tbTo.Text) - Int32.Parse(tbFrom.Text))} -c copy \"{filePath + $"\\{videoTitle}{Path.GetExtension(fileName)}"}\"").Start();*/
-                await conversions.Start();
-                File.Delete(fileName);
-                pbProgress.IsIndeterminate = false;
-            }
+                bool anyChecked = false;
+                Dispatcher.Invoke(() =>
+                {
+                    if (cboxCut.IsChecked == true || cboxConvert.IsChecked == true || cboxBitrate.IsChecked == true)
+                        anyChecked = true;
+                });
+                
+                if (anyChecked)
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        pbProgress.IsIndeterminate = true;
+                        textStatus.Text = "Getting FFmpeg";
+                    });
+                    //Dispatcher.Invoke(new Action(() => { }), DispatcherPriority.Render);
+                    if (!File.Exists(runningPath + "\\ffmpeg.exe"))
+                        await FFmpegDownloader.GetLatestVersion(FFmpegVersion.Full, runningPath);
+                    Dispatcher.Invoke(() => { textStatus.Text = "Processing"; });
+                    //Dispatcher.Invoke(new Action(() => { }), DispatcherPriority.Render);
+                    var conversions = FFmpeg.Conversions.New();
+                    Dispatcher.Invoke(() =>
+                    {
+                        if (cboxCut.IsChecked == true)
+                            conversions.AddParameter($"-ss {TimeSpan.FromSeconds(Int32.Parse(tbFrom.Text))}");
+                        conversions.AddParameter($"-i \"{fileName}\"");
+                        if (cboxConvert.IsChecked == true)
+                            conversions.SetOutput($"{filePath}\\{videoTitle}{cbConvertType.Text}");
+                        else
+                            conversions.SetOutput($"{filePath}\\{videoTitle}{Path.GetExtension(fileName)}");
+                        if (cboxCut.IsChecked == true)
+                            conversions.AddParameter($"-to {TimeSpan.FromSeconds(Int32.Parse(tbTo.Text) - Int32.Parse(tbFrom.Text))}");
+                        if (cboxBitrate.IsChecked == true)
+                            conversions.AddParameter($"-b {tbBitrate.Text}k");
+                    });
+                    
+                    /*
+                    if (cboxConvert.IsChecked == true && cboxCut.IsChecked == false)
+                        await conversions.AddParameter($"-i \"{fileName}\" \"{filePath}\\{videoTitle}{cbConvertType.Text}\"").Start();
+                    if (cboxConvert.IsChecked == true && cboxCut.IsChecked == true)
+                        await conversions.AddParameter($"-ss {TimeSpan.FromSeconds(Int32.Parse(tbFrom.Text))} -i \"{fileName}\" -to {TimeSpan.FromSeconds(Int32.Parse(tbTo.Text) - Int32.Parse(tbFrom.Text))} -c copy \"{filePath + $"\\{videoTitle}{cbConvertType.Text}"}\"").Start();
+                    if (cboxConvert.IsChecked == false && cboxCut.IsChecked == true)
+                        await conversions.AddParameter($"-ss {TimeSpan.FromSeconds(Int32.Parse(tbFrom.Text))} -i \"{fileName}\" -to {TimeSpan.FromSeconds(Int32.Parse(tbTo.Text) - Int32.Parse(tbFrom.Text))} -c copy \"{filePath + $"\\{videoTitle}{Path.GetExtension(fileName)}"}\"").Start();*/
+                    await conversions.Start();
+                    File.Delete(fileName);
+                    Dispatcher.Invoke(() => { pbProgress.IsIndeterminate = false; });
+                }
+            });
             textStatus.Text = "Idle";
             pbProgress.Visibility = Visibility.Hidden;
             bGo.IsEnabled = true;
